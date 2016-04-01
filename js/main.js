62025de377456d3787ea2b45fe9872a97afdfb71
esotcm <pauliusshiu@gmail.com>
2016-04-01 13:27:56 +0100

    lel

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Paul on 4/1/2016.
+ */
+
+/*! Main */
+jQuery(document).ready(function($) {
+
+    // Fixa navbar ao ultrapassa-lo
+    var navbar = $('#navbar-main'),
+        distance = navbar.offset().top,
+        $window = $(window);
+
+    $window.scroll(function() {
+        if ($window.scrollTop() >= distance) {
+            navbar.removeClass('navbar-fixed-top').addClass('navbar-fixed-top');
+            $("body").css("padding-top", "70px");
+        } else {
+            navbar.removeClass('navbar-fixed-top');
+            $("body").css("padding-top", "0px");
+        }
+    });
+});
